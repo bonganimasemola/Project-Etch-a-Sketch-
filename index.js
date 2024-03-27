f4a8c0fb21b6b86524be5b9421286402da2ab9da
bonganimasemola <bonganimasemola@icloud.com>
2024-03-27 15:29:05 +0200

    create grid logic complete, user input and hover effect complete

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-document.addEventListener('DOMContentLoaded', () =>  {
+document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('gridContainer');
     const changeSizeBtn = document.getElementById('changeSize');
 
     function createGrid(size) {
-        container.innerHTML = '';
-        let smallerSquare = 960 / size;
-        container.style.width = '960px';
+        container.innerHTML = ''; 
+        let squareSize = 960 / size; 
+        container.style.width = '960px'; 
 
         for (let i = 0; i < size * size; i++) {
             const square = document.createElement('div');
-            square.style.width = `${smallerSquare}px`;
-            square.style.height = `${smallerSquare}px`;
-            
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.classList.add('gridSquare');
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black'; 
+            });
+            container.appendChild(square);
+        }
     }
+
+    changeSizeBtn.addEventListener('click', () => {
+        let newSize = prompt('Enter new size per side for the grid (max 100)', 16);
+        newSize = Math.min(100, Math.max(1, newSize)); 
+        createGrid(newSize);
+    });
+
+    createGrid(16); 
+});
