892614cd6ba53d3dc6a95bdab8e7e7addffa86c7
bonganimasemola <bonganimasemola@icloud.com>
2024-03-27 18:38:15 +0200

    added changeColor function

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
             square.style.width = `${squareSize}px`;
             square.style.height = `${squareSize}px`;
             square.classList.add('gridSquare');
-            square.addEventListener('mouseover', () => {
-                square.style.backgroundColor = 'black'; 
-            });
+            square.dataset.darkness = 0; 
+            square.addEventListener('mouseover', () => changeColor(square));
             container.appendChild(square);
+        }
+    }
+
+    function changeColor(square) {
+        if (square.dataset.darkness == 0) {
+            
+            const [r, g, b] = [0, 1, 2].map(() => Math.floor(Math.random() * 256));
+            square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            square.dataset.darkness = 1;
+        } else {
+            
+            let currentDarkness = Math.min(10, parseInt(square.dataset.darkness));
+            let factor = 1 - (currentDarkness * 0.1);
+            square.style.filter = `brightness(${factor})`;
+            square.dataset.darkness = currentDarkness + 1;
         }
     }
 
